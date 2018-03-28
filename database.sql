21115a64250960aa55f71a8954e08ca5952eb594
Kerry Burns <30881654+kburns17@users.noreply.github.com>
2018-03-28 18:10:20 -0500

    Complete

--- database.sql
+++ database.sql
@@ -11,8 +11,10 @@ UPDATE "accounts" SET "account_balance" = '10.00' WHERE ("account_balance" = 0 a
 SELECT * FROM "accounts" WHERE "transactions_attempted" >= 9;
 
 -- 5.Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
+SELECT * FROM "accounts" ORDER BY "account_balance" DESC LIMIT 3;
 
 -- 6.Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
+SELECT * FROM "accounts" ORDER BY "account_balance" ASC LIMIT 3;
 
 -- 7.Get all users with account balances that are more than $100.
 SELECT * FROM "accounts" WHERE  "account_balance" > 100;
@@ -20,3 +22,4 @@ SELECT * FROM "accounts" WHERE  "account_balance" > 100;
 -- 8.Add a new record.
 INSERT INTO accounts (username, city, transactions_completed, transactions_attempted, account_balance) VALUES ('kerry', 'minneapolis', 12, 12, 1717.17);
 -- 9.The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: Delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+SELECT * FROM "accounts" WHERE ("city" = 'miami' AND "transactions_completed" < 5) OR ("city" = 'phoenix' AND "transactions_completed" < 5);
